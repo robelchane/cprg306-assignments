6994e3fc8103824ab1a1ef5cade908910122dc06
robelchane <robel.chane@edu.sait.ca>
2024-01-20 21:38:16 -0700

    Complete week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+import Link from "next/link"
+export default function StudentInfo(){
+    return <main>
+        <p>Robel Chane</p>
+        <Link href="https://github.com/robelchane/cprg306-assignments">https://github.com</Link>
+    </main>
+}
