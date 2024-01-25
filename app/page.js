a05f1078c7b1b2f2ce23900bbd2410188fcaa517
robelchane <robel.chane@edu.sait.ca>
2024-01-25 01:43:19 -0700

    FinishAssignment3

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import Link from "next/link"
+
 export default function page(){
   return <main>
-    <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href="week-2">Week 2 Assignment</Link>
+    <div className="m-24">
+      <h1 className="mb-5 text-3xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+      <p><Link href="week-2">Week 2 Assignment</Link></p>
+      <p><Link href="week-3">Week 3 Assignment</Link></p>
+    </div>
+
     </main>
   
 }
