0ba5463813e9290464955a61e83f2f2d89ea53d1
robelchane <robel.chane@edu.sait.ca>
2024-04-19 23:41:01 -0600

    Update shopping-list-service.js

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,16 +1,17 @@
 import { db } from "../_utils/firebase";
-import { collection, getDocs, addDoc, deleteDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc,  deleteItem } from "firebase/firestore";
 
 export async function getItems(userId) {
     const postSnapshot = await getDocs(collection(db, `users/${userId}/items`));
     return postSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-}
-
-export async function addItem(userId, item) {
+  }
+  
+  export async function addItem(userId, item) {
     const docRef = await addDoc(collection(db, `users/${userId}/items`), item);
     return docRef.id;
-}
-
-export async function deleteItem(userId, itemId) {
+  }
+  
+  export async function deleteItem(userId, itemId) {
     await deleteDoc(doc(db, `users/${userId}/items/${itemId}`));
-}
+  }
+  
